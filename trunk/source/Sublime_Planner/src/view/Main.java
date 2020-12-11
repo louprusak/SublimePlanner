d37978217e178aaaa245e0fa33d99cb77540b2e6
lorusak <loup.rusak@etu.uca.fr>
2020-12-11 10:00:32 +0100

    modif vue accueil

--- trunk/source/Sublime_Planner/src/view/Main.java
+++ trunk/source/Sublime_Planner/src/view/Main.java
@@ -21,7 +21,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/layout/ToDoListe.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/layout/Accueil.fxml"));
         primaryStage.setTitle("Sublime Planner");
 
         primaryStage.setScene(new Scene(root,H_SIZE,V_SIZE));
